d8069d74a7ca51c2d5d1e177ffd83ae52680632b
tanakakjs <oneok0813@gmail.com>
2020-09-05 07:53:48 +0900

    タイトルへんこう

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container max-w-screen-md mx-auto py-4 px-4 md:px-0">
-    <header class="pb-8"><nuxt-link to="/">empitsu</nuxt-link></header>
+    <header class="pb-8"><nuxt-link to="/">empitsu.log()</nuxt-link></header>
     <nuxt />
   </div>
 </template>
