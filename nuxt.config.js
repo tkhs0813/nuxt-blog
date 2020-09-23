26c59fb6ca7c6e6242074fc63995a5a2c6b7f442
tanakakjs <oneok0813@gmail.com>
2020-09-24 00:08:38 +0900

    og

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,15 +4,61 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'empitsu.log()',
+    htmlAttrs: { lang: 'ja' },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       {
         hid: 'description',
         name: 'description',
-        content: process.env.npm_package_description || '',
+        content: 'empitsuの個人サイト',
       },
+      { property: 'og:locale', hid: 'og:locale', content: 'ja_JP' },
+      { property: 'og:type', hid: 'og:type', content: 'blog' },
+      { property: 'og:title', hid: 'og:title', content: 'empitsu.log()' },
+      {
+        property: 'og:description',
+        hid: 'og:description',
+        content: 'empitsuの個人サイト',
+      },
+      {
+        property: 'og:site_name',
+        hid: 'og:site_name',
+        content: 'empitsu.log()',
+      },
+      {
+        name: 'twitter:card',
+        hid: 'twitter:card',
+        content: 'summary_large_image',
+      },
+      {
+        property: 'og:url',
+        hid: 'og:url',
+        content: 'https://empitsu.dev/',
+      },
+      // TODO: 画像作る
+      // {
+      //   property: 'og:image',
+      //   hid: 'og:image',
+      //   content: '',
+      // },
+      {
+        name: 'twitter:description',
+        hid: 'twitter:description',
+        content: 'empitsuの個人サイト',
+      },
+      {
+        name: 'twitter:title',
+        hid: 'twitter:title',
+        content: 'empitsu.log()',
+      },
+      // TODO: 画像作る
+      // {
+      //   name: 'twitter:image',
+      //   hid: 'twitter:image',
+      //   content: '',
+      // },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
